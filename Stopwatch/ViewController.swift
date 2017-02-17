5d892d32496ef0ad00ff4baddae96880cfe38bf9
tfloydmews <tfloydmews@Taylors-iMac.local>
2017-02-16 21:36:48 -0600

    Lab 3 Stopwatch
    Lab 3

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,6 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var elapsedTime: UILabel!
+    var time = 0
+    var displayValue = 0
+    var timer = Timer()
+    var timerRunning = false
+    @IBAction func startTimer(_ sender: UIBarButtonItem) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.increment), userInfo: nil, repeats: true)
+        timerRunning = true
+    }
+    
+    func increment() {
+        time += 1
+        elapsedTime!.text = "\(time)"
+    }
+    
+    @IBAction func stopTimer(_ sender: UIBarButtonItem) {
+        if timerRunning {
+            timer.invalidate()
+            timerRunning = false
+        }
+        else {
+            return
+        }
+    }
+    
+    @IBAction func resetTimer(_ sender: UIBarButtonItem) {
+        if timerRunning {
+            timer.invalidate()
+            time = 0
+            elapsedTime!.text = "0"
+        }
+        else{
+            time = 0
+            elapsedTime!.text = "0"
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
